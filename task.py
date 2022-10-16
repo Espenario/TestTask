c536701233978b6f5565dc8e92250bbd237826ff
Espenario <86924641+Espenario@users.noreply.github.com>
2022-10-16 11:31:32 +0300

    Диагностические функции

---
+++ task.py
@@ -0,0 +1,35 @@
+
+def calc_digit_sum(x, numbers):
+    sum = 0
+    if x == 0:
+        return 0
+    while x > 0:
+        sum += int(x % 10)
+        x /= 10
+        if x in numbers:
+            return numbers[x] + sum
+    return sum
+
+def count_groups_size_from_zero(n_customers):
+    groups = {}
+    numbers = {}
+    for i in range(n_customers + 1):
+        group_num = calc_digit_sum(i, numbers)
+        if group_num in groups:
+            groups[group_num] += 1
+        else:
+            groups[group_num] = 1
+    print(groups)
+
+def count_groups_size(n_first_id, n_customers):
+    groups = {}
+    numbers = {}
+    for i in range(n_first_id, n_customers + 1):
+        group_num = calc_digit_sum(i, numbers)
+        if group_num in groups:
+            groups[group_num] += 1
+        else:
+            groups[group_num] = 1
+    print(groups)
+
+count_groups_size(10000, 453212)
